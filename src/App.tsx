c48f1163a03b6efe1de3f81273ce80afde34e451
17101965 <vzvagolsky@gmail.com>
2024-06-19 09:01:37 +0200

    First

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,19 @@
-import { createContext, useState } from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Main from "./components/Main";
-import { navItems } from "./utils/constants";
-
-export interface IPageContext {
-  page: string,
-  setPage: (newValue: string) => void
-}
-// 1. Создание контекста
-export const PageContext = createContext<IPageContext | null>(null);
+import Counter from "./components/Counter";
+import Library from "./components/Library";
+import Sandwich from "./components/Sandwich";
+import Layout from "./components/Layout";
 
 function App() {
-  const [page, setPage] = useState(navItems[0]);
-
-  console.log(page);
-
   return (
-    <>
-      {/* 2. Передача данных контекстом */}
-      <PageContext.Provider value={{ page, setPage }}>
-        <Header />
-        <Main />
-      </PageContext.Provider>
-      <Footer />
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Counter />}/>
+        <Route path="/sandwich" element={<Sandwich />}/>
+        <Route path="/library" element={<Library />}/>   
+      </Route>
+    </Routes>
   );
 }
 
--- src/components/StarWars.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const StarWars = () => {
-  return (
-    <div>StarWars</div>
-  )
-}
-
-export default StarWars
